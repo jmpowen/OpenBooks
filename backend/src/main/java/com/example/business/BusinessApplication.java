38ed9c35e933eb5095436157e20bce92a4f78e1a
Sebastian Kazun <smkazun@iastate.edu>
2019-11-01 22:19:30 -0500

    name change

--- backend/src/main/java/com/example/business/BusinessApplication.java
+++ backend/src/main/java/com/example/business/BusinessApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class BusinessApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
+        SpringApplication.run(BusinessApplication.class, args);
     }
 
 }
