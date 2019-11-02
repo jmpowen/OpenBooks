c20e608b23572c1fc3a106e1035f2a990b4f7dbd
wasartin <wasartin@iastate.edu>
2019-11-02 03:05:34 -0500

    Finalizing the SQL Schema

--- backend/resources/hackathon.sql
+++ backend/resources/hackathon.sql
@@ -1,16 +1,14 @@
-use hack;
 
-SET FOREIGN_KEY_CHECKS = 0;
+SET FOREIGN_KEY_CHECKS = 0;                     /* Turns off checks for Foreign keys which helps for dropping tables easier*/
 DROP TABLE IF EXISTS user;
 DROP TABLE IF EXISTS donor;
 DROP TABLE IF EXISTS charity_volunteer;
-
 DROP TABLE IF EXISTS charity;
 DROP TABLE IF EXISTS donation;
 DROP TABLE IF EXISTS volunteers;
 DROP TABLE IF EXISTS expense;
 
-SET FOREIGN_KEY_CHECKS = 1;
+SET FOREIGN_KEY_CHECKS = 1;                     /* Turns on checks for foreign keys, which needs to be on to add all these*/
 CREATE TABLE user (
   user_id int(11) NOT NULL AUTO_INCREMENT,
   user_email varchar(128),
@@ -28,7 +26,7 @@ CREATE TABLE donor (
     FOREIGN KEY(donor_id) REFERENCES user(user_id) ON DELETE CASCADE
 );
 
--- TODO: See relationship with charity table
+    /* TODO: See relationship with charity table */
 CREATE TABLE charity_volunteer (
 	volunteer_id int(11),
 
@@ -38,21 +36,21 @@ CREATE TABLE charity_volunteer (
 CREATE TABLE charity(
 	charity_id INT(11) NOT NULL AUTO_INCREMENT,	/* Unique Id for each Charity Organization */
 	charity_name VARCHAR(128),  				/* After less than two minutes of googling, 46 chars was the longest I could find */
-    charity_nick_name VARCHAR(66),
+    charity_nickname VARCHAR(66),
     charity_description VARCHAR(1200), 			/* This is obtained by the average paragraph length (200 words) * the average word length (6 characters) */
     charity_trending_rank INT(11),
-    /* ToDo: charity_links JSON ? */
+    charity_links LONGTEXT,						/* LONGTEXT is an alias for JSON, this is used for compatibility issues.*/
     charity_balance DECIMAL(19,4), 				/* This datatype is recommended by the first stackoverflow via google 'best datatype money SQL' */
 	PRIMARY KEY(charity_id)
 );
 
 ALTER TABLE charity AUTO_INCREMENT=0;
 
--- TODO: see relationship with expense table
+    /* TODO: see relationship with expense table */
 CREATE TABLE donation (
 	donation_id int(11) NOT NULL AUTO_INCREMENT,
-    amount decimal(19, 4),
-    comment varchar(256),
+    donation_amount decimal(19, 4),
+    donation_comment varchar(256),
     charity_id int(11),
     donor_id int(11),
 
@@ -68,12 +66,13 @@ CREATE TABLE volunteers (
     volunteer_id int(11),
     position varchar(128),
     salary decimal(19, 4),
-    
+
     PRIMARY KEY(charity_id, volunteer_id),
     FOREIGN KEY(charity_id) REFERENCES charity(charity_id) ON DELETE CASCADE,
     FOREIGN KEY(volunteer_id) REFERENCES charity_volunteer(volunteer_id) ON DELETE CASCADE
 );
 
+    /* ToDo: FOREIGN KEY(expense_account_id) */
 CREATE TABLE expense (
 	expense_id INT(11) NOT NULL AUTO_INCREMENT,
 	charity_id INT(11),
@@ -81,10 +80,8 @@ CREATE TABLE expense (
     expense_description VARCHAR(1200),
     expense_amount DECIMAL(19, 4),
     expense_date DATE,
-    expense_account_id INT(11),
     PRIMARY KEY(expense_id),
     FOREIGN KEY(charity_id) REFERENCES charity(charity_id)
-    /* ToDo: FOREIGN KEY(expense_account_id) */
 );
 
 ALTER TABLE expense AUTO_INCREMENT=0;
